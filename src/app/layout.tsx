66351b0505dc6d924e041bcd8dd747a4da2c7b93
alseong <als6251@outlook.com>
2025-08-26 08:58:29 -0400

    Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Script from "next/script";
 import { TempoInit } from "@/components/tempo-init";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <TempoInit />
+        <Analytics />
       </body>
     </html>
   );
